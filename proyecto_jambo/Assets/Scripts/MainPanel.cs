f99e73c80b8a1d1c8811af37d6393f6c3a3ce9a2
maxi condori <profekennedy5040@gmail.com>
2023-11-05 17:27:31 -0300

    se añadio la funcion en los botones
    en los paneles se añadio la funcion para ir y volver a distintos paneles

--- proyecto_jambo/Assets/Scripts/MainPanel.cs
+++ proyecto_jambo/Assets/Scripts/MainPanel.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainPanel : MonoBehaviour
 {
-  
+    [Header("opciones")]
+    public Slider volumen;
+    [Header("Paneles")]
+    public GameObject backgroundPanel;
+    public GameObject mainPanel;
+    public GameObject opcionPanel;
+
+    public void OpenPanel(GameObject panel)
+    {
+        backgroundPanel.SetActive(false);
+        mainPanel.SetActive(false);
+        opcionPanel.SetActive(false);
+
+        panel.SetActive(true);
+    }
 }
